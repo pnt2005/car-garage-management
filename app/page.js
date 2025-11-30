5ab17761a4b1c52b1fcf7d31d405f3417265fd40
hintran24 <hientrannnn24@gmail.com>
2025-11-30 20:54:40 +0700

    fix page(app)

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
 import BaoCaoDoanhSo from "./baocaodoanhso/page";
 import BaoCaoTon from "./baocaoton/page";
+import PhieuSuaChua from "./phieusuachua/page";
+import TienCong from "./tiencong/page";
 
 export default function Home() {
   return (
